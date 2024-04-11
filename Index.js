c087747db8fcdb72b88d19ef96393bab2467ebfa
nextsukhorut <nextsukhorutchenko@gmail.com>
2024-04-11 06:22:32 +0300

    add first task

--- Index.js
+++ Index.js
@@ -1,1 +1,17 @@
-some text!
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true,
+ or a "No" string for false.
+*/
+
+function boolToString(bool) {
+	//Use the ternaty operator for a concise opproach
+	return bool ? "yes" : "No";
+}
+
+// Example usage
+const isTrue = true;
+const isFalse = false;
+
+console.log(boolToString(isTrue));
+console.log(boolToString(isFalse));
